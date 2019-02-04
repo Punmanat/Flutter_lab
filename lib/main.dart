c369a22f33122b02b1ff67fe3d54f3ca70c85ba0
Punmanat <60070053@kmitl.ac.th>
2019-02-04 10:40:28 +0700

    Test Widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,43 +17,73 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyHomePageState();
-  }
-}
-
-class MyHomePageState extends State<MyHomePage> {
-  int counter = 0;
+class MyHomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello, World....."),
-      ),
-      body: Center(child: 
-      Column(
-        children: <Widget>[
-          Text("$counter"),
-          Text("-------1-------"),
-          Text("-------1-------"),
-          Text("-------1-------"),
-        ],
-      )
-      ),
-      floatingActionButton: IconButton(
-        icon: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-          print("Counter : $counter");
-        },
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Tabbar"),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.account_box),text: "account_box",),
+              Tab(icon: Icon(Icons.camera), text: "camera",),
+              Tab(icon: Icon(Icons.calendar_today),text:"Calendar")
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.account_box),
+            Icon(Icons.camera),
+            Icon(Icons.calendar_today),
+          ],
+        ),
       ),
     );
   }
+
 }
+
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     return MyHomePageState();
+//   }
+// }
+
+// class MyHomePageState extends State<MyHomePage> {
+//   int counter = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Hello, World....."),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text("$counter"),
+//             Text("-------1-------"),
+//             Text("-------1-------"),
+//             Text("-------1-------"),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Icon(Icons.add),
+//         onPressed: () {
+//           setState(() {
+//             counter++;
+//           });
+//           print("Counter : $counter");
+//         },
+//       ),
+//     );
+//   }
+// }
 
 // class MyHomePage extends StatelessWidget {
 //   int counter = 0;
