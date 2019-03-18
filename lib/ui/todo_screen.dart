fb2574ffec0f39f700c44f4785798f0d563634f3
Punmanat <60070053@kmitl.ac.th>
2019-03-18 11:03:20 +0700

    center

--- lib/ui/todo_screen.dart
+++ lib/ui/todo_screen.dart
@@ -9,58 +9,62 @@ class TodoScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("TodoScreen"),
       ),
-      body: Column(
-        children: <Widget>[
-          RaisedButton(
-            child: Text("Open DB"),
-            onPressed: () {
-              todo.open("todo.db");
-            },
-          ),
-          RaisedButton(
-            child: Text("Insert"),
-            onPressed: () async {
-              Todo data = Todo();
-              data.title = "Test";
-              data.done = false;
-              Todo result = await todo.insert(data);
-              print(result.title);
-              // todo.insert(data).then((result)=>{
-              // });
-            },
-          ),
-          RaisedButton(
-            child: Text("Get"),
-            onPressed: () async {
-              Todo data = await todo.getTodo(2);
-              print(data.toMap());
-            },
-          ),
-          RaisedButton(
-            child: Text("Update"),
-            onPressed: () async {
-              Todo newData = Todo();
-              newData.id = 1;
-              newData.title = "New Test";
-              newData.done = true;
+      body: Container(
+        alignment: FractionalOffset.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Open DB"),
+              onPressed: () {
+                todo.open("todo.db");
+              },
+            ),
+            RaisedButton(
+              child: Text("Insert"),
+              onPressed: () async {
+                Todo data = Todo();
+                data.title = "Test";
+                data.done = false;
+                Todo result = await todo.insert(data);
+                print(result.title);
+                // todo.insert(data).then((result)=>{
+                // });
+              },
+            ),
+            RaisedButton(
+              child: Text("Get"),
+              onPressed: () async {
+                Todo data = await todo.getTodo(2);
+                print(data.toMap());
+              },
+            ),
+            RaisedButton(
+              child: Text("Update"),
+              onPressed: () async {
+                Todo newData = Todo();
+                newData.id = 1;
+                newData.title = "New Test";
+                newData.done = true;
 
-              int result = await todo.update(newData);
-              print(result);
-            },
-          ),
-          RaisedButton(
-            child: Text("Delete"),
-            onPressed: () async {
-              int result = await todo.delete(1);
-            },
-          ),
-          RaisedButton(
-            child: Text("Close DB"),
-            onPressed: () {
-              todo.close();
-            },
-          )
-        ],
+                int result = await todo.update(newData);
+                print(result);
+              },
+            ),
+            RaisedButton(
+              child: Text("Delete"),
+              onPressed: () async {
+                int result = await todo.delete(1);
+              },
+            ),
+            RaisedButton(
+              child: Text("Close DB"),
+              onPressed: () {
+                todo.close();
+              },
+            )
+          ],
+        ),
       ),
     );
   }
